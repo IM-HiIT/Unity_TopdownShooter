b8436751c9ee1837eb354607695f47162780e8c7
I Munster <ian.Munster@StichtingHeldenInIT.nl>
2024-03-11 18:44:14 +0100

    Updated to 2017.4.40f1 LTS

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -3,14 +3,14 @@ using System.Collections;
 
 public class EnemyMovement : MonoBehaviour {
 
-	private NavMeshAgent _navMeshAgent;
+	private UnityEngine.AI.NavMeshAgent _navMeshAgent;
 	private GameObject _playerObj;
 	// Use this for initialization
 	//void Start () {
 	
 	//}
 	void Awake(){
-		_navMeshAgent = GetComponent<NavMeshAgent> ();
+		_navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
 		_playerObj = GameObject.FindGameObjectWithTag ("Player");
 	}
 	// Update is called once per frame
